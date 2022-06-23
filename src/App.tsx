000f4b5ea96a3fe9b183bb84d5610d52c9f90a7a
henrique32174 <henrique32174@fiec.edu.br>
2022-06-23 10:36:58 -0300

    aula-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-function App() {
- 
+import { gql, useQuery } from '@apollo/client'
 
-  return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello word</h1>
+const GET_LESSONS_QUERY =   gql`
+  query{
+    lessons{
+      id
+      title
+
+      teacher{
+      name
+    }
+    }    
+  }
+
+`
+interface Lesson{
+  id: string;
+  title: string;
+}
+
+
+function App() { 
+
+const { data } = useQuery <{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
+ return (
+ <ul>
+    {data?.lessons.map(lesson => {
+      return <li key={lesson.id}>{lesson.title}</li>
+    })}
+  </ul>
+  
   )
 }
 
